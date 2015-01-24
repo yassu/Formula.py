04e9b7ed24643942cb6d7f28c0c09c5f0d5ff0ab
yassu <mathyassu@gmail.com>
2015-01-24 23:45:40 +0900

    MathItem class with simple list structure

--- src/formula.py
+++ src/formula.py
@@ -1,29 +1,26 @@
-class MathItem:
+class MathItem(object):
     def __init__(self, data):
         self._data = data
         self._before = None
         self._afters = []
 
     @property
-    def before(self):
-        return self._before
-
-    @property
     def data(self):
         return self._data
+
+    @property
+    def before(self):
+        return self._before
 
     def _set_before(self, before):
         self._before = before
 
-    def append(self, item):
-        self._afters.append(item)
-        item._set_before(self)
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self._data == other._data
+    def append(self, after):
+        self._afters.append(after)
+        after._set_before(self)
 
     def __getitem__(self, ind):
-        return self._afters[0]
+        return self._afters[ind]
 
-    def __ne__(self, other):
-        return not (self == other)
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.data == other.data
--- test/formula_test.py
+++ test/formula_test.py
@@ -3,27 +3,40 @@ path.append('src')
 from formula import MathItem
 from unittest import TestCase
 
+
 class MathItemTest(TestCase):
-    def mathitem_create_test(self):
-        math = MathItem('data')
+    def create_test(self):
+        math = MathItem('123')
 
-    def mathitem_set_before_test(self):
-        math = MathItem('1')
-        math._set_before(MathItem('2'))
-        assert(math.before == MathItem('2'))
+    def data_test(self):
+        math = MathItem(1)
+        assert(math.data == 1)
 
-    def mathitem_append_test(self):
-        math0 = MathItem('0')
-        math1 = MathItem('1')
-        math0.append(math1)
-        math0[0] == math1
-        assert(math1.before == math0)
+    def before_test(self):
+        math = MathItem(1)
+        math2 = MathItem(2)
+        math2._before = math
+        assert(math2.before == math)
 
-    def mathitem_eq_test1(self):
-        assert(MathItem(2) == MathItem(2))
+    def after_test(self):
+        math = MathItem(1)
+        math2 = MathItem(2)
+        math._afters.append(math2)
+        assert(math[0] == math2)
 
-    def mathitem_eq_test2(self):
+    def append_test(self):
+        math = MathItem(1)
+        math2 = MathItem(2)
+        math.append(math2)
+        assert(math[0] == math2)
+        assert(math2.before == math)
+
+    def eq_test(self):
+        math1 = MathItem(2)
+        math2 = MathItem(2)
+        assert(math1 == math2)
+
+    def neq_test(self):
         math1 = MathItem(2)
         math2 = MathItem(3)
-        print(math1._data != math2._data)
-        assert(MathItem(2) != MathItem(3))
+        assert(math1 != math2)
